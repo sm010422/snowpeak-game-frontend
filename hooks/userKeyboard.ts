571b8f3b72d6d7e6e8af2f618e23209a090d984c
Park Sang Min <sm0104224@gmail.com>
2025-12-30 22:24:06 +0900

    feat: userkeyborad.ts update

--- hooks/userKeyboard.ts
+++ hooks/userKeyboard.ts
@@ -1,22 +1,21 @@
-// hooks/useKeyboard.ts
 import { useEffect, useRef } from 'react';
 
 export const useKeyboard = () => {
   const keys = useRef<{ [key: string]: boolean }>({});
 
   useEffect(() => {
-    const down = (e: KeyboardEvent) => { 
-        keys.current[e.key.toLowerCase()] = true; 
-        keys.current[e.key] = true; // 대소문자 이슈 방지용 안전장치
+    const down = (e: KeyboardEvent) => {
+      keys.current[e.key.toLowerCase()] = true;
+      keys.current[e.key] = true; // 대소문자 이슈 방지용 안전장치
     };
-    const up = (e: KeyboardEvent) => { 
-        keys.current[e.key.toLowerCase()] = false; 
-        keys.current[e.key] = false;
+    const up = (e: KeyboardEvent) => {
+      keys.current[e.key.toLowerCase()] = false;
+      keys.current[e.key] = false;
     };
-    
+
     window.addEventListener('keydown', down);
     window.addEventListener('keyup', up);
-    
+
     return () => {
       window.removeEventListener('keydown', down);
       window.removeEventListener('keyup', up);
